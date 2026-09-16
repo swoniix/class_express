4770d80282ae12516dd8d62660def9c2a6466f18
swoniix <sonyabl909@gmail.com>
2026-09-16 14:09:54 +0300

    commit 2.0

--- src/server.ts
+++ src/server.ts
@@ -13,9 +13,12 @@ app.use(express.static("public"))
 app.use(express.json()) //body -> json
 //middleware - попередній обробник
 
-app.engine("ejs", ejs)
-app.set("views", path.resolve(__dirname, "views"))
-app.set("view engine", "ejs")
+app.set("view engine", ejs);
+app.set("views", path.join(__dirname, "../views"));
+
+app.get('/', (req,res)=>{
+res.render("pages/home")
+})
 
 app.use("/books", router)
 app.get('/',(req,res)=>{
