175a484d844faca2a395e643edda3bb6b4c573fb
swoniix <sonyabl909@gmail.com>
2026-09-16 15:04:51 +0300

    final_site vibes

--- src/data/books.ts
+++ src/data/books.ts
@@ -3,38 +3,38 @@ import type { BookType } from "../types/BookType.js";
 export const books: Array<BookType> = [
   {
     id: 1,
-    title: "Кобзар",
+    title: "Chanel №1",
     price: 250,
     is_active: true,
     image: "rab.jpg"
   },
   {
     id: 2,
-    title: "Тигролови",
+    title: "Life lessons",
     price: 320,
     is_active: true,
     image: "rab2.jpg"
   },
   {
     id: 3,
-    title: "1984",
+    title: "1000-7",
     price: 400,
     is_active: true,
     image:"rab3.jpg"
   },
   {
     id: 4,
-    title: "Гаррі Поттер і філософський камінь",
+    title: "Breakfast in bed",
     price: 450,
     is_active: false,
-    image:"rab3.jpg"
+    image:"rab4.jpg"
   },
   {
     id: 5,
-    title: "Маленький принц",
+    title: "Small cheese",
     price: 280,
     is_active: true, 
-    image: "rab.jpg"
+    image: "rab5.jpg"
  }
 ];
  
